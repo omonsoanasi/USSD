031eb265862c4df23c73d3de6efe3850d0089780
omonsoanasi <emmanuelomonso@gmail.com>
2021-10-21 13:13:34 +0300

    deploy USSD

--- index.php.php
+++ index.php.php
@@ -57,3 +57,29 @@
     echo $response;
 
 ?>
+
+<body>
+</body>
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyAOMLeFcunbcTYVNks3bI1AEK32cb0uZwc",
+    authDomain: "ussdomonso.firebaseapp.com",
+    projectId: "ussdomonso",
+    storageBucket: "ussdomonso.appspot.com",
+    messagingSenderId: "759581808375",
+    appId: "1:759581808375:web:53b4373b0b12f100b065f2",
+    measurementId: "G-VVLYE4J122"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
